e6fc49b0e72134eea12b1800b14d304168515ad2
Goran Kekez <gorankekez@gmail.com>
2023-07-10 13:31:18 +0200

    last commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
 let addPhoneBtn = document.getElementById('add-phone');
+let saveChangesBtn = document.getElementById('save-changes')    
 
 if (addPhoneBtn) {
     addPhoneBtn.addEventListener('click', savePhone);
+}
+
+if (saveChangesBtn) {
+  saveChangesBtn.addEventListener('click', saveChanges);
 }
 
 function savePhone() {
@@ -20,4 +25,32 @@ function savePhone() {
             window.location.href = '/';
         }
     })
+}
+
+function saveChanges() {
+    let inputId = document.querySelector("[name = 'id']");
+    let inputBrand = document.querySelector("[name = 'brand']");
+    let inputModel = document.querySelector("[name = 'model']");
+    let inputPrice = document.querySelector("[name = 'price']");
+    let inputCount = document.querySelector("[name = 'count']");
+
+    API.updatePhone({
+      id: inputId.value,
+      brand: inputBrand.value,
+      model: inputModel.value,
+      price: inputPrice.value,
+      count: inputCount.value
+    }).then((res) => {
+      if (res.status === 200) {
+        window.location.href = '/';
+      }
+    });
+}
+
+function deletePhone(id) {
+    API.deletePhone(id).then(res => {
+        if (res.status === 205) {
+            window.location.reload();
+        }
+    });
 }
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 let db = [];
 
@@ -11,16 +12,56 @@ app.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+  db = require('./phone.json');
+  res.render('index', {phone: db});
 });
 
 app.get('/add', (req, res) => {
   res.render('add');
 });
 
+app.get('/edit-delete', (req, res) => {
+  db = require('./phone.json');
+  res.render('edit-delete', {phone: db});
+});
+
+app.get('/edit/:id', (req, res) => {
+  db = require('./phone.json');
+  let phone = db.find((el) => el.id === parseInt(req.params.id))
+  res.render('edit', {phone});
+});
+
+app.post('/edit', (req, res) => {
+  let id = parseInt(req.body.id);
+  db = require('./phone.json');
+  let foundIndex = null;
+  db.find((el, index) => {
+    if (el.id === id) {
+      foundIndex = index;
+      return;
+    }
+  });
+  db[foundIndex] = req.body;
+  fs.writeFileSync('phone.json', JSON.stringify(db));
+  res.send('OK');
+});
+
+app.delete('/edit-delete/:id', (req, res) => {
+  db = require('./phone.json');
+  let id = parseInt(req.params.id);
+  let new_db = db.filter((el) => el.id !== id)
+  fs.writeFileSync('phone.json', JSON.stringify(new_db));
+  res.status(205).send('OK');
+});
+
 app.put('/add', (req, res) => {
   //console.log(req.body); // Ovde dolaze podaci iz forme add.ejs
-  db.push(req.body);
+  let phone = {...req.body};
+  //let phone = Object.assign(req.body); // ovo je isto kao ovo iznad
+  db = require('./phone.json');
+  phone.id = db.length
+  db.push(phone);
+  fs.writeFileSync('phone.json', JSON.stringify(db));
   res.send('OK');
 });
 
